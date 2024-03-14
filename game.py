4c2d69d0c506a2726ce70a5290f3432cd7eaca3c
echeadle <echeadle@gmail.com>
2024-03-14 10:12:19 -0600

    Add play loop

--- game.py
+++ game.py
@@ -3,3 +3,10 @@ import pygame
 pygame.init()
 
 window = pygame.display.set_mode([600, 600])
+
+playing = True
+
+while playing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playing=False
